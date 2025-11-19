3183476d734ef0d2737e660beed374901fae1f1c
adsoftsito <adsoft@live.com.mx>
2025-11-19 11:07:46 -0600

    Update flowers_client.py

--- flowers_client.py
+++ flowers_client.py
@@ -10,29 +10,28 @@ from numpy import asarray
 SERVER_URL = 'http://localhost:8501/v1/models/flowers-model:predict'
 
 #image = '/home/adsoft/.keras/datasets/flower_photos/roses/10090824183_d02c613f10_m.jpg'
-image = '/home/codespace/.keras/datasets/flower_photos/flower_photos/tulips/100930342_92e8746431_n.jpg'
+image = '/home/adsoft/.keras/datasets/flower_photos/tulips/100930342_92e8746431_n.jpg'
 #image = '/home/adsoft/.keras/datasets/flower_photos/sunflowers/1008566138_6927679c8a.jpg'
 score = 0
 
 
 def main():
+  img = Image.open(image)
 
-  img = Image.open(image).resize((64, 64))
-  img_array = np.asarray(img, dtype=np.float32)
+#  img = Image.open(image)
+  #img = Image.open('dog.png')
+  img = img.resize((64, 64))
+  img_array = asarray(img)
 
-  # TensorFlow Serving requires batch dimension
-  payload = {
-    "instances": [img_array.tolist()]
-  }
+  img = np.expand_dims(img_array, 0).tolist()
+  predict_request = json.dumps({'instances': img })
 
   # Send few actual requests and report average latency.
   total_time = 0
   num_requests = 1
   index = 0
   for _ in range(num_requests):
-    #response = requests.post(SERVER_URL, json=predict_request)
-    response = requests.post(SERVER_URL, json=payload)
- 
+    response = requests.post(SERVER_URL, data=predict_request)
     response.raise_for_status()
     total_time += response.elapsed.total_seconds()
     prediction = response.json()['predictions']
