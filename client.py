03e3fd7f1dae366ae63e80153145f16efb42a5cb
adsoftsito <adsoft@live.com.mx>
2025-11-19 02:09:53 +0000

    update client

--- client.py
+++ client.py
@@ -5,34 +5,45 @@ import numpy as np
 from PIL import Image
 import requests
 from numpy import asarray
+import cv2
 
-#SERVER_URL = 'https://tensorflow-linear-model-0khx.onrender.com/v1/models/flowers-model:predict'
+
+#SERVER_URL = "https://flowers-serving-l4tu.onrender.com/v1/models/flowers_model:predict"
+
+
 SERVER_URL = 'http://localhost:8501/v1/models/flowers-model:predict'
 
-#image = '/home/adsoft/.keras/datasets/flower_photos/roses/10090824183_d02c613f10_m.jpg'
-image = '/home/codespace/.keras/datasets/flower_photos/flower_photos/tulips/100930342_92e8746431_n.jpg'
-#image = '/home/adsoft/.keras/datasets/flower_photos/sunflowers/1008566138_6927679c8a.jpg'
 score = 0
+image = '/home/codespace/.keras/datasets/flower_photos/flower_photos/tulips/100930342_92e8746431_n.jpg'
 
 
 def main():
+  img = Image.open(image)
+  #img = Image.open('dog.png')
+  img = img.resize((64, 64))
+  img_array = asarray(img)
 
-  img = Image.open(image).resize((64, 64))
-  img_array = np.asarray(img, dtype=np.float32)
+  #img = np.expand_dims(img_array, 0).tolist()
+  #predict_request = json.dumps({'instances': img })
+  img_array = img_array.astype(np.float32) / 255.0
+  img_array = np.expand_dims(img_array, 0)
 
-  # TensorFlow Serving requires batch dimension
-  payload = {
-    "instances": [img_array.tolist()]
+  predict_request = {
+    "instances": [
+        {
+            "inputs": img_array.tolist()
+        }
+    ]
   }
-
+ 
   # Send few actual requests and report average latency.
   total_time = 0
   num_requests = 1
   index = 0
   for _ in range(num_requests):
-    #response = requests.post(SERVER_URL, json=predict_request)
-    response = requests.post(SERVER_URL, json=payload)
- 
+    #response = requests.post(SERVER_URL, data=predict_request)
+    response = requests.post(SERVER_URL, json=predict_request)
+
     response.raise_for_status()
     total_time += response.elapsed.total_seconds()
     prediction = response.json()['predictions']
